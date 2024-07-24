7cb2b12fa0c4c88eca024b97e7e334e6fd69831a
allangabr <allanmediogames@gmail.com>
2024-07-24 14:53:50 -0300

    Department entity class

--- src/db/DB.java
+++ src/db/DB.java
@@ -17,7 +17,7 @@ public class DB {
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
-				String url = props.getProperty("urlbd");
+				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
